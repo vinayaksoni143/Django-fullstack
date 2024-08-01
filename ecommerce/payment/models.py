9ac8d2bfc652e39beea688b79890aa0ee2b5ff84
= <=>
2024-08-01 17:39:32 +0530

    order model added

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from store.models import Product
+
 # Create your models here.
 
 class ShippingAddress(models.Model):
@@ -25,4 +27,19 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return 'Shipping Address - '+str(self.id)
+
+
+class Order(models.Model):
+
+    full_name = models.CharField(max_length = 300)
+    email = models.CharField(max_length = 255)
+    shipping_address = models.TextField(max_length = 1000)
+    amount_paid = models.DecimalField(max_digits = 8, decimal_places = 2)
+    date_ordered = models.DateTimeField(auto_now_add = True)
+
+    # foreign key
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order - #'+str(self.id)
 
